b1c5d8f3ddcb2a2d56450ea6b663fac86250d5c6
Navanath Bhosale <navanathb@bsf.io>
2023-04-14 18:54:25 +0530

    chore: build

--- tests/docker/initial-setup.sh
+++ tests/docker/initial-setup.sh
@@ -4,14 +4,10 @@ echo "Our docker file"
 echo "Setup Astra"
 wp theme activate astra
 
-echo "Rewrite permalinks..."
-wp rewrite structure /%postname%/
-
-echo "Installing basic-auth to interact with the API..."
-wp plugin install https://github.com/WP-API/basic-auth/archive/master.zip --activate
-wp plugin activate basic-auth
-
 # echo "Activate <your-extension>"
 # wp plugin activate your-extension
+
+echo "Rewrite permalinks..."
+wp rewrite structure /%postname%/ --hard --quiet
 
 echo "Success! Your E2E Test Environment is now ready."
