c51cfac572a97e0641de78cb135175df25215bc2
Simran <simranc@bsf.io>
2023-06-07 16:40:52 +0530

    added html-2 color, link color and font size

--- tests/play/specs/header-builder/elements/html-2/link-color.spec.js
+++ tests/play/specs/header-builder/elements/html-2/link-color.spec.js
@@ -1,25 +1,25 @@
 const { test, expect } = require( '@playwright/test' );
 const { setCustomizeSettings } = require( '../../../../utils/customize' );
 const { setBrowserViewport } = require( '../../../../utils/set-browser-viewport' );
-test.describe( 'HTML 1 link color verification', () => {
+test.describe( 'HTML 2 link color verification', () => {
 		const colorSettings = {
-			'header-html-1': '<a href="https://wpastra.com/">HTML-1 link color</a>',
-			'header-html-1link-color': {
-				desktop: 'rgb(11, 177, 115)',
+			'header-html-2': '<a href="https://wpastra.com/">HTML-2 link color</a>',
+			'header-html-2link-color': {
+				desktop: 'rgb(21, 177, 115)',
 				tablet: 'rgb(131, 11, 166)',
 				mobile: 'rgb(131, 11, 166)',
 			},
 			'header-desktop-items': {
 				primary: {
 					primary_center: {
-						0: 'html-1',
+						0: 'html-2',
 					},
 				},
 			},
 			'header-mobile-items': {
 				primary: {
 					primary_center: {
-						0: 'html-1',
+						0: 'html-2',
 					},
 				},
 			},
@@ -27,25 +27,25 @@ test.describe( 'HTML 1 link color verification', () => {
         test.beforeAll( async ( { baseURL } ) => {
             await setCustomizeSettings( colorSettings, baseURL );
         } );
-        test( 'HTML 1 link color on the front end.', async ({ page }) => {
-            await test.step('HTML 1 color', async () => {
+        test( 'HTML 2 link color on the front end.', async ({ page }) => {
+            await test.step('HTML 2 color', async () => {
                 await page.goto('/');
                 await page.waitForTimeout(5000);
-                const htmlLinkColor = await page.$eval('.ast-header-html-1 a',
+                const htmlLinkColor = await page.$eval('.ast-header-html-2 a',
                 el => window.getComputedStyle(el).color);
-                await expect(htmlLinkColor).toBe(colorSettings['header-html-1link-color'].desktop);
+                await expect(htmlLinkColor).toBe(colorSettings['header-html-2link-color'].desktop);
     
                 await setBrowserViewport(page, "medium");
                 await page.waitForTimeout(5000);
-                const htmlLinkColorTablet = await page.$eval('.ast-header-html-1 a',
+                const htmlLinkColorTablet = await page.$eval('.ast-header-html-2 a',
                 el => window.getComputedStyle(el).color);
-                await expect(htmlLinkColorTablet).toBe(colorSettings['header-html-1link-color'].tablet);
+                await expect(htmlLinkColorTablet).toBe(colorSettings['header-html-2link-color'].tablet);
     
                 await setBrowserViewport(page, "small");
                 await page.waitForTimeout(5000);
-                const htmlLinkColorMobile = await page.$eval('.ast-header-html-1 a',
+                const htmlLinkColorMobile = await page.$eval('.ast-header-html-2 a',
                 el => window.getComputedStyle(el).color);
-                await expect(htmlLinkColorMobile).toBe(colorSettings['header-html-1link-color'].mobile);
+                await expect(htmlLinkColorMobile).toBe(colorSettings['header-html-2link-color'].mobile);
             });
         } );
     } );
